240b18616bca7920f9efd9ce7334a1abc8492805
Connor Hallman <challman@umich.edu>
2018-04-11 19:49:14 -0400

    Initial Commit

---
+++ eecs398hw13/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  eecs398hw13
+//
+//  Created by Connor Hallman on 4/11/18.
+//  Copyright © 2018 Connor Hallman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ eecs398hw13/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  eecs398hw13
+//
+//  Created by Connor Hallman on 4/11/18.
+//  Copyright © 2018 Connor Hallman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
